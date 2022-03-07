f30c817ace871ab5eb10cfe0039419c9686d0878
chengweg <chengweg@andrew.cmu.edu>
2022-03-07 00:15:03 -0800

    pip to null

--- chaos-environment/intro.md
+++ chaos-environment/intro.md
@@ -1,4 +1,4 @@
 In this scenario, we created a user for you.
 Your username is "ubuntu". Your password is "password".
 
-kubernetes
+minikube
--- chaos-environment/setup.sh
+++ chaos-environment/setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-useradd -m -d /home/ubuntu -G sudo -p $(openssl passwd -1 password) ubuntu && cd /home/ubuntu
-sudo chmod 666 /var/run/docker.sock
-cd /home/ubuntu && su ubuntu
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikube
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
-sudo apt-get install apt-transport-https --yes
-echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+useradd -m -d /home/ubuntu -G sudo -p $(openssl passwd -1 password) ubuntu && cd /home/ubuntu > /dev/null
+sudo chmod 666 /var/run/docker.sock > /dev/null
+cd /home/ubuntu && su ubuntu > /dev/null
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 > /dev/null
+sudo install minikube-linux-amd64 /usr/local/bin/minikube > /dev/null
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" > /dev/null
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl > /dev/null
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add - > /dev/null
+sudo apt-get install apt-transport-https --yes > /dev/null
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list > /dev/null
 sudo apt-get update > /dev/null
 sudo apt-get install helm
-minikube start 
+minikube start
