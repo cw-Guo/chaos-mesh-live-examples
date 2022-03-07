1eabd1a3f12036577c3d55f1214f371c5c82d81d
chengweg <chengweg@andrew.cmu.edu>
2022-03-07 00:35:28 -0800

    remove update log

--- chaos-environment/background.sh
+++ chaos-environment/background.sh
@@ -10,6 +10,6 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 curl https://baltocdn.com/helm/signing.asc | sudo apt-key add - 
 sudo apt-get install apt-transport-https --yes  
 echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list 
-sudo apt-get update 
+sudo apt-get update > /dev/null
 sudo apt-get install helm
 echo "done" >> /root/katacoda-background-finished
