b278cc9f78235af9c65a3766d56fa6a84e650907
chengweg <chengweg@andrew.cmu.edu>
2022-03-09 15:10:05 -0800

    one node test

--- environment/foreground.sh
+++ environment/foreground.sh
@@ -1,6 +1,6 @@
-sleep 1; wait.sh
+init.sh
 # cd /home/ubuntu && su ubuntu 
 # minikube start -n 2
 # minikube start
 # sleep 2;
-echo "welcome to chaos mesh live example"
+echo "welcome to chaos mesh live example!"
