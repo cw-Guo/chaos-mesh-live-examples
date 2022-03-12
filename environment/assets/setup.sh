752bd0e759a225186e385db7a8e9ef236109bf55
chengweg <chengweg@andrew.cmu.edu>
2022-03-12 11:52:12 -0800

    add proxy

--- environment/assets/setup.sh
+++ environment/assets/setup.sh
@@ -3,3 +3,4 @@
 # kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.5.0/aio/deploy/recommended.yaml --validate=false
 # kubectl proxy --address='0.0.0.0' --port=8002 --accept-hosts='.*' &
 minikube dashboard --url=true &
+kubectl proxy --address='0.0.0.0' --disable-filter=true
