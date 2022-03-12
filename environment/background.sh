8f3735f73acba4e2148aae66fb152c333f4b6824
chengweg <chengweg@andrew.cmu.edu>
2022-03-11 22:36:47 -0800

    add chaos mesh install

--- environment/background.sh
+++ environment/background.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 echo "done" >> /root/katacoda-finished
+# update helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+# wait cluster
 sleep 20s
+
+# install chaos-mesh
+helm repo add chaos-mesh https://charts.chaos-mesh.org
+kubectl create ns chaos-testing
+helm install chaos-mesh chaos-mesh/chaos-mesh -n=chaos-testing --version 2.1.3
+
 echo "done" >> /root/katacoda-background-finished
