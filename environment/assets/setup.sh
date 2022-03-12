d32de72694de05f4b0798832a95534764c28fc60
chengweg <chengweg@andrew.cmu.edu>
2022-03-12 11:48:39 -0800

    add dashboard

--- environment/assets/setup.sh
+++ environment/assets/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # install dashboard
 # kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.5.0/aio/deploy/recommended.yaml --validate=false
-kubectl proxy --address='0.0.0.0' --port=8002 --accept-hosts='.*' &
+# kubectl proxy --address='0.0.0.0' --port=8002 --accept-hosts='.*' &
+minikube dashboard --url=true &
