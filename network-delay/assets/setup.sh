2339cde9d7caf36e0a8ffbbe990b0d3b5cf63d9b
chengweg <chengweg@andrew.cmu.edu>
2022-05-28 11:49:03 -0700

    add dashboard user

--- network-delay/assets/setup.sh
+++ network-delay/assets/setup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.5.0/aio/deploy/recommended.yaml
 kubectl apply -f dashboard-adminuser.yaml
 kubectl proxy --address='0.0.0.0' --disable-filter=true &
