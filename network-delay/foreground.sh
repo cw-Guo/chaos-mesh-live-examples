e42ceaee182c292248fe8dfffcd295567bcaf523
chengweg <chengweg@andrew.cmu.edu>
2022-05-28 12:05:27 -0700

    fix foreground

--- network-delay/foreground.sh
+++ network-delay/foreground.sh
@@ -1,6 +1,2 @@
 sleep 1;wait.sh
-# cd /home/ubuntu && su ubuntu 
-# minikube start -n 2
-# minikube start
-# sleep 2;
-# echo "welcome to chaos mesh live example!"
+echo "welcome to chaos mesh live example!"
