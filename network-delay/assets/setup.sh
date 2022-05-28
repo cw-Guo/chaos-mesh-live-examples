7d0999ab2745eec7f717d6a48859ba8df12befbe
chengweg <chengweg@andrew.cmu.edu>
2022-05-28 11:59:14 -0700

    redirect output

--- network-delay/assets/setup.sh
+++ network-delay/assets/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.5.0/aio/deploy/recommended.yaml
 kubectl apply -f dashboard-adminuser.yaml
-kubectl proxy --address='0.0.0.0' --disable-filter=true &
+kubectl proxy --address='0.0.0.0' --disable-filter=true & 2>/dev/null
+sleep 1
